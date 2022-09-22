89db635248f1a646686fe744dbf072ea6507b746
PABLO COGUA CASTRO <u1701463@unimilitar.edu.co>
2022-09-21 23:19:27 -0500

    Second

--- pages/stock.js
+++ pages/stock.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 
 const options = {
@@ -15,7 +14,8 @@ export default function Stock(props) {
     const [lastPrice, setLastPrice] = useState(0);
     const [changeAmount, setChangeAmount] = useState(0);
     const [changePercentage, setchangePercentage] = useState(0);
-    const url = `https://twelve-data1.p.rapidapi.com/time_series?symbol=${props.ticker}&interval=15min&outputsize=30&format=json`;
+    const ticker = props.currency === "USD" ? 'OTC Pink: PKKFF' : 'CSE: PKK';
+    const url = `https://twelve-data1.p.rapidapi.com/time_series?symbol=PKKFF&interval=15min&outputsize=30&format=json`;
 
     useEffect(() => {
         fetch(url, options)
@@ -29,16 +29,28 @@ export default function Stock(props) {
                 setchangePercentage(() => {
                     return (((changeStockAmount * 100) / lastPrice).toFixed(2));
                 });
+                props.onDateChange(response.values[0].datetime);
             })
             .catch(err => console.error(err));
     }, []);
 
     return (
-        <div>
-            <h1>Stock Performance</h1>
-            <p>OTC Pink: PKKFF</p>
-            <p>${price} USD</p>
-            <p>{changeAmount} {changePercentage}%</p>
+        <div className='stock-info'>
+            <div className='stock-ticker _26pt-eyebrow'>{ticker}</div>
+            <div className='stock-price-container'>
+                <div className='inline-text'>$</div>
+                <div className='inline-text'>{price}</div>
+                <div className='inline-text currency-font'>{props.currency}</div>
+            </div>
+            <div className='stock-change _17pt-menu-top'>
+                <div className='stock-price-change'>
+                    <div>{changeAmount}</div>
+                </div>
+                <div className='stock-percentage-change'>
+                    <div className='inline-text'>{changePercentage}</div>
+                    <div className='inline-text'>%</div>
+                </div>
+            </div>
         </div>
     );
 
