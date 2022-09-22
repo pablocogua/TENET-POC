833e53406fcb23792ef7a55f86f024da2092b220
PABLO COGUA CASTRO <u1701463@unimilitar.edu.co>
2022-09-22 08:11:09 -0500

    clean up

--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,6 @@ import React, { useState, useEffect } from 'react';
 import StockContainer from './stock-container';
 
 export default function Home() {
-
-  const [date, setDate] = useState("");
-
-  function handleDateChange(date) {
-    setDate(() => {
-      const updateDate = new Date(date);
-      let year = updateDate.getUTCFullYear();
-      let month = updateDate.toLocaleString('default', { month: 'long' });
-      let day = updateDate.getUTCDate();
-      let hour = updateDate.getHours();
-      let minutes = updateDate.getMinutes();
-
-      return `${month} ${day}, ${year} ${hour}:${minutes}`;
-    });
-  }
-
   return (
     <div className='investor-news-stock-section'>
       <div className='stock-quote'>
